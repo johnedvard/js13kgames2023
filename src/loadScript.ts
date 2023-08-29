3e5eaf217f35a33be85813bbe5313609ae4346a2
John Edvard Reiten <johnedvard89@gmail.com>
2023-08-30 00:56:10 +0900

    fix: add keystore

--- src/loadScript.ts
+++ src/loadScript.ts
@@ -1,8 +1,14 @@
+import { setKeyStore } from './nearConfig';
+
 export function loadScript(url: string): Promise<any> {
   return new Promise((resolve) => {
     if ((<any>window).nearApi) resolve('');
+
     const script = document.createElement('script');
     script.onload = () => {
+      const nearApi = (<any>window).nearApi;
+      const myKeyStore = new nearApi.keyStores.BrowserLocalStorageKeyStore();
+      setKeyStore(myKeyStore);
       resolve('');
     };
     script.src = url;
