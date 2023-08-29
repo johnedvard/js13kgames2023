e5b1add45c07bf161936ae3fc7eb01c15f2ff175
John Edvard Reiten <johnedvard89@gmail.com>
2023-08-29 23:39:06 +0900

    feat: add proxy to call paras marketplace

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,18 @@
 import { js13kViteConfig } from 'js13k-vite-plugins';
 import { defineConfig } from 'vite';
 
-export default defineConfig(
-  js13kViteConfig({
+export default defineConfig({
+  server: {
+    port: 9090,
+    proxy: {
+      '/api': {
+        target: 'https://api-v2-mainnet.paras.id/token-series?collection_id=samurai-sam-by-johnedvardnear',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  },
+  ...js13kViteConfig({
     closureOptions: false,
-  })
-);
+  }),
+});
